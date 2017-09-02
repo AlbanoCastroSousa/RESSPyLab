2131dea8d5ea888cea9b46ff9c47189c8bda3397
Albano de Castro e Sousa <Albano@albano.home>
2017-09-02 22:31:43 +0200

    took out import resspylab

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,7 @@ project_root = os.path.dirname(cwd)
 # version is used.
 sys.path.insert(0, project_root)
 
-import RESSPyLab
+#import RESSPyLab
 
 # -- General configuration ---------------------------------------------
 
